1a223577871aec3b4f2b2d4885f1a5ff715047a6
Lewis Webb <webblcw23@hotmail.com>
2025-11-19 03:35:14 +0000

    remove sub id

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "91c0fe80-4528-4bf2-9796-5d0f2a250518"
+ # subscription_id = "91c0fe80-4528-4bf2-9796-5d0f2a250518"
 }
 
 
